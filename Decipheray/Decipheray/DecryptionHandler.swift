99d3c6039e9581174c8ca13d52a43870941f418d
tidalboot <tidalboot@googlemail.com>
2015-04-30 20:15:33 +0100

    Small refactor/tidy

--- Decipheray/Decipheray/DecryptionHandler.swift
+++ Decipheray/Decipheray/DecryptionHandler.swift
@@ -16,39 +16,38 @@ public class DecryptionHandler {
     
     public func decipherText (textToDecipher: String) -> String {
         
-        var ayayIndex: String.Index?
-        var textToBeChecked = NSMutableString(string: textToDecipher)
         var newText = textToDecipher
         var arrayOfStrings = [String]()
         
         if textToDecipher.rangeOfString("ayay") != nil {
-            ayayIndex = textToDecipher.rangeOfString("ayay")?.endIndex
+            var textToBeChecked = NSMutableString(string: textToDecipher)
             var valueToSearch = "ayay"
             var replaceWith = "ay$"
             var myRegEx = NSRegularExpression(pattern: valueToSearch, options: .CaseInsensitive , error: nil)
             
             myRegEx?.replaceMatchesInString(textToBeChecked, options: nil, range: NSMakeRange(0, textToBeChecked.length), withTemplate: replaceWith)
             newText = textToBeChecked as String
-            arrayOfStrings = newText.componentsSeparatedByString("ay")
-            
-        } else {
-            arrayOfStrings = newText.componentsSeparatedByString("ay")
         }
+
+        arrayOfStrings = newText.componentsSeparatedByString("ay")
         
         for (index, string) in enumerate(arrayOfStrings) {
             if string != "" {
-                let stringLength = count(string)
-                var shortenedString = string.stringByPaddingToLength(stringLength, withString: string, startingAtIndex: 1)
+                var shortenedString = string.stringByPaddingToLength(count(string), withString: string, startingAtIndex: 1)
+                //Stores last character in the string
                 var lastCharacter = shortenedString[shortenedString.endIndex.predecessor()]
+                //Removes the last chracter from the string
                 shortenedString = dropLast(shortenedString)
-                
+                //And the puts the last character back in the string but now at the beginning
                 shortenedString.insert(lastCharacter, atIndex: shortenedString.startIndex)
                 
+                //Finds whether the $ flag is present in the string
                 if string.rangeOfString("$") != nil {
+                    //Removes the flag
                     shortenedString = dropLast(shortenedString)
+                    //And then inserts the removed 'a' and 'y' where the $ flag was previously
                     shortenedString.insert("a", atIndex: shortenedString.endIndex)
                     shortenedString.insert("y", atIndex: shortenedString.endIndex)
-
                 }
                 arrayOfStrings[index] = shortenedString
             }
@@ -56,12 +55,14 @@ public class DecryptionHandler {
         
         var finalArray = [String]()
         
+        //Finally removes any blank objects in the array
         for (index, string) in enumerate(arrayOfStrings) {
             if string != "" {
                 finalArray.append(string)
             }
         }
         
+        //Concatonates all of the objects in the array to a string and inserts a space between each
         var newString = " ".join(finalArray)
         return newString
     }
